45914a74d0af42db17bcf60216362c4073abbd89
myungsworld <myungsworld@gmail.com>
2020-10-01 23:42:36 +0900

    [ADD] URLSession

---
+++ Tutorials/Tutorials/URLSession.swift
@@ -0,0 +1,63 @@
+//
+//  URLSession.swift
+//  Tutorials
+//
+//  Created by myungsworld on 2020/10/01.
+//
+
+import SwiftUI
+
+struct Response : Codable {
+    var result : [Result]
+}
+
+struct Result : Codable {
+    var trackId : Int
+    var trackName : String
+    var collectionName : String
+}
+
+struct URLSession: View {
+    
+    @State var results = [Result]()
+    
+    var body: some View {
+        List(results, id: \.trackId) { item in
+            VStack(alignment : .leading) {
+                Text(item.trackName)
+                    .font(.headline)
+                
+                Text(item.collectionName)
+            }
+        }
+        .onAppear(perform: loadData)
+    }
+    
+    func loadData() {
+        guard let url = URL(string: "https://itunes.apple.com/search?term=taylor+swift&entity=song") else {
+            print("Invalid URL")
+            return
+        }
+        
+        let request = URLRequest(url: url)
+        
+        URLSession.shared.dataTask(with: request) { data, response, error in
+            if let data = data {
+                if let decodedResponse = try? JSONDecoder().decode(Response.self, from: data) {
+                    DispatchQueue.main.async {
+                        self.results = decodedResponse.results
+                    }
+                    return
+                }
+            }
+            
+            print("Fetch failed: \(error?.localizedDescription ?? "Unknown error")")
+        }.resume()
+    }
+}
+
+struct URLSession_Previews: PreviewProvider {
+    static var previews: some View {
+        URLSession()
+    }
+}
